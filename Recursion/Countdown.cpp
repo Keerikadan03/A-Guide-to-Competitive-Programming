2b180062e4538dd475385015998dceff8c8f6232
Keerikadan03 <fenseraju@gmail.com>
2023-11-25 15:23:09 +0530

    understanding as to when and how the recursive functions exits the stack and modified code for a count up as well

--- Recursion/Countdown.cpp
+++ Recursion/Countdown.cpp
@@ -6,7 +6,8 @@ void cd(int n){
         return;
     }
     cout<< n<<"\n";
-    cd(n-1);
+    cd(n-1);        //if this line is swapped with line 8 it will do count up instead of countdown
+            //this is because the print is given after all the recursions are called while exiting the function calls.
 }
 
 int main(){
