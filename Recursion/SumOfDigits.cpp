3b4c38ba3ae22fd533f0268ac0ae109b2b9dcd53
Keerikadan03 <fenseraju@gmail.com>
2023-11-25 16:09:20 +0530

    added product of digits also to git

--- Recursion/SumOfDigits.cpp
+++ Recursion/SumOfDigits.cpp
@@ -8,8 +8,16 @@ int sum(int n){
     return (n%10) + sum(n/10);
 }
 
+int prod(int n){
+    if(n%10 == n){
+        return n;
+    }
+    return (n%10) * prod(n/10);
+}
+
 int main(){
-    int n = 9;
-    cout<<sum(n);
+    int n = 0;
+    cout<<prod(n)<<"\n";
+    cout<<sum(n)<<"\n";
     return 0;
 }
